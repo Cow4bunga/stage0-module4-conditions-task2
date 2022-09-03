b56652277c90be1763854edd0afe129168e8f424
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-09-03 14:02:59 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int max=first;
+        if(second>first)
+            max=second;
+        System.out.println(max);
     }
 }
